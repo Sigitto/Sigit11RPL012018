9f1e8a983700d8d2893ee6b35ea1dcf9d16020de
Sigitto <sigithadi234@gmail.com>
2020-08-06 18:48:16 +0700

    add login and cardview

---
+++ app/src/main/java/com/example/sigit11rpl012018/CardView.java
@@ -0,0 +1,14 @@
+package com.example.sigit11rpl012018;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adapter);
+    }
+}
--- app/src/main/java/com/example/sigit11rpl012018/MainActivity.java
+++ app/src/main/java/com/example/sigit11rpl012018/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.sigit11rpl012018;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText txtusername;
+    EditText txtpassword;
+    Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtusername = findViewById(R.id.txtusername);
+        txtpassword = findViewById(R.id.txtpassword);
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtusername.getText().toString().isEmpty()&& txtpassword.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Jangan di kosongkan", Toast.LENGTH_SHORT).show();
+                }else if(txtusername.getText().toString().equals("Sigit") && txtpassword.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Jangan Dikosongkan", Toast.LENGTH_SHORT).show();
+                }else if(txtusername.getText().toString().isEmpty() && txtpassword.getText().toString().equals("Uwaw")){
+                    Toast.makeText(MainActivity.this, "Jangan Dikosongkan", Toast.LENGTH_SHORT).show();
+                }else if(txtusername.getText().toString().equals("Sigit") && (txtpassword.getText().toString().equals("Uwaw")) ){
+                    Toast.makeText(MainActivity.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this, CardView.class);
+                    startActivity(i);
+                }else{
+                    Toast.makeText(MainActivity.this, "Login Gagal", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
